b07d811a83f650e4496042fc2c7a08167a254d8d
Peter J. Jones <peter@jonesbunch.com>
2025-12-04 17:19:58 +0100

    nix: Modernize the Nix configuration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Peter's image scripts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs, ... }:
@@ -11,32 +11,29 @@
       supportedSystems = nixpkgs.lib.platforms.unix;
 
       # Function to generate a set based on supported systems:
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      # Attribute set of nixpkgs for each system:
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs { inherit system; });
+      each = f:
+        nixpkgs.lib.genAttrs supportedSystems (system:
+          let pkgs = import nixpkgs { inherit system; };
+          in f pkgs system);
     in
     {
-      packages = forAllSystems (system: {
-        image-scripts = import ./. { pkgs = nixpkgsFor.${system}; };
+      packages = each (pkgs: system: {
         default = self.packages.${system}.image-scripts;
+        image-scripts = pkgs.callPackage ./. { };
       });
 
       overlays = {
         default = final: prev: {
           pjones = (prev.pjones or { }) // {
-            image-scripts = self.packages.${prev.system}.image-scripts;
+            image-scripts = self.packages.${prev.stdenv.hostPlatform.system}.image-scripts;
           };
         };
       };
 
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system}; in {
-          default = pkgs.mkShell {
-            inputsFrom = builtins.attrValues self.packages.${system};
-          };
-        });
+      devShells = each (pkgs: system: {
+        default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
     };
 }
